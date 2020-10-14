012de78ca1ab0fce24fc3780b93e13e57e97ea89
zhangqinglin <zhangqinglin@megvii.com>
2020-10-14 20:38:03 +0800

    quick sort with bug

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -2,36 +2,34 @@ import random
 from typing import List
 
 
-def quick_sort(arr: List[int], l: int, r: int):
-    """
-    Parameters:
-
-    arr List[int]: array to sort
-
-    l int: [l,r) lb of arr
-
-    r int: [l,r) rb of arr 
-    """
-    n = r - l
-    if n <= 1:
+def partation(arr: List[int], l: int, r: int):
+    if r - l + 1 <= 1:
         return
+    piviot = arr[(l + r) >> 1]
 
-    mid = (l+r) // 2
-    anchor = arr[mid]
-    arr[mid], arr[r-1] = arr[r-1], arr[mid]
+    # WHY need this switch?
+    arr[r], arr[(l + r) >> 1] = piviot, arr[r]
+
     index = l
-    for i in range(l, r-1):
-        if arr[i] <= anchor:
+    for i in range(l, r):
+        if arr[i] <= piviot:
             arr[index], arr[i] = arr[i], arr[index]
             index += 1
-    arr[r-1], arr[index] = arr[index], arr[r-1]
-    # 重点:skip anchor本身在的那一个元素
-    quick_sort(arr, l, index)
-    quick_sort(arr, index+1, r)
+
+    # WHY switch back here?
+    arr[r], arr[index] = arr[index], arr[r]
+    
+    return index
+
+
+def quick_sort(arr: List[int], l: int, r: int):
+    k = partation(arr, l, r)
+    partation(arr, l, k)
+    partation(arr, k + 1, r)
 
 
 n = 10
-
+random.seed(0)
 arr = [random.randint(0, n) for i in range(n)]
-quick_sort(arr, 0, n)
+quick_sort(arr, 0, n-1)
 print(arr)
