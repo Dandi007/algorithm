3241b6baf25f2cfc2093379f3029ee4276ab86ba
zhangqinglin <zhangqinglin@megvii.com>
2020-10-14 20:49:29 +0800

    fix quick sort bug and add comment

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -3,11 +3,12 @@ from typing import List
 
 
 def partation(arr: List[int], l: int, r: int):
-    if r - l + 1 <= 1:
-        return
     piviot = arr[(l + r) >> 1]
 
     # WHY need this switch?
+    # 如果piviot恰好是最大的,那么不做switch的话,这次partation将不会对整个序列做任何调整
+    # 通过这次强行移动,确保了返回的index上一定是这次partation最大的元素,就可以在外层的sort
+    # 来跳过这个元素,保证每一次sort都会起码在待排序序列中去除一个元素
     arr[r], arr[(l + r) >> 1] = piviot, arr[r]
 
     index = l
@@ -16,20 +17,22 @@ def partation(arr: List[int], l: int, r: int):
             arr[index], arr[i] = arr[i], arr[index]
             index += 1
 
-    # WHY switch back here?
     arr[r], arr[index] = arr[index], arr[r]
-    
     return index
 
 
 def quick_sort(arr: List[int], l: int, r: int):
+    if r - l <= 1:
+        return
     k = partation(arr, l, r)
-    partation(arr, l, k)
-    partation(arr, k + 1, r)
+    # 在待排序序列中去除起码一个元素
+    # 以及递归调用sort而非partition
+    quick_sort(arr, l, k - 1)
+    quick_sort(arr, k + 1, r)
 
 
 n = 10
 random.seed(0)
 arr = [random.randint(0, n) for i in range(n)]
-quick_sort(arr, 0, n-1)
+quick_sort(arr, 0, n - 1)
 print(arr)
